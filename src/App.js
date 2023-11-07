4bdeba7d3360d0c9a7950d2aec4e1d5393ff2ec1
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2023-11-07 14:27:41 -0500

    images removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import EventList from "./components/EventList";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <EventList />
+    </div>
+  );
 };
 
 export default App;
