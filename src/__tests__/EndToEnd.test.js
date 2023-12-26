ab4773c762595eec9f682477d0c00284510598d8
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2023-12-26 14:12:10 -0500

    endtoendtest failing

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,17 +1,27 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer';
 
-describe("show/hide an events details", () => {
-  test("An event element is collapsed by default", async () => {
-    const browser = await puppeteer.launch();
+describe('show/hide event details', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  });
 
-    const page = await browser.newPage();
-    await page.goto("http://localhost:3000/letsmeet");
-
-    await page.waitForSelector(".event");
-
-    const eventDetails = await page.waitForSelector(".details");
-
-    expect(eventDetails).toBeNull();
+  afterAll(() => {
     browser.close();
+  });
+
+  test('An event element is collapsed by default', async () => {
+    const eventDetails = await page.$('.event .details);
+    expect(eventDetails).toBeNull();
+  });
+
+  test('User can expand an event to see details', async () => {
+    await page.click('.event .details-btn');
+    const eventDetails = await page.$('.event .details);
+    expect(eventDetails).toBeDefined();
   });
 });
