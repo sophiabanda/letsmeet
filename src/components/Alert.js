024c0309721b300a2c639dd9a92fd5c9ebee0789
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-01-08 13:11:55 -0500

    added missing css props

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -9,6 +9,16 @@ class Alert extends Component {
   getStyle = () => {
     return {
       color: this.color,
+      backgroundColor: this.bgColor,
+      borderWidth: "2px",
+      borderStyle: "solid",
+      fontWeight: "bolder",
+      borderRadius: "7px",
+      borderColor: this.color,
+      textAlign: "center",
+      fontSize: "12px",
+      margin: "10px 0",
+      padding: "10px",
     };
   };
 
@@ -25,7 +35,7 @@ class InfoAlert extends Alert {
   constructor(props) {
     super(props);
     this.color = "rgb(0, 0, 255)";
-    this.backgroundColor = "rgb(220, 220, 255)";
+    this.bgColor = "rgb(220, 220, 255)";
   }
 }
 class WarningAlert extends Alert {
