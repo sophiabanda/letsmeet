98cad1f0e5f3244f6e6e5f7fec4a95092c32a0d9
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-01-12 16:10:09 -0500

    removed comments

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -16,17 +16,7 @@ export const NumberOfEvents = ({
       setCurrentNoE(number);
     }
     setErrorAlert(errorText);
-
-    // const NOE = parseInt(number, 10);
-    // setCurrentNoE(NOE);
   };
-
-  //mentor: try typing text in the input to see if it breaks:
-  //It did not break, and I changed the input type to number from text
-  //leaving as text: parse code left NaN if text was input, vs
-  // nothing happening at all. With number type, nothing happen with text, but
-  //it hardly allows you to type. parse code was also throwing an error
-  //which I'd have to recreate to recall or understand
 
   useEffect(() => {
     setNumber(currentNoE);
